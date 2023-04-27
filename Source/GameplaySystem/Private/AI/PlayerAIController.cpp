073dc0ee969e7093807b3e862ddeef2a22172684
TheBitFossil <5188146+TheBitFossil@users.noreply.github.com>
2023-04-27 22:59:47 +0200

    AIController: template

---
+++ Source/GameplaySystem/Private/AI/PlayerAIController.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/PlayerAIController.h"
+
+APlayerAIController::APlayerAIController()
+{
+	bWantsPlayerState = true;
+}
---
+++ Source/GameplaySystem/Public/AI/PlayerAIController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "PlayerAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEPLAYSYSTEM_API APlayerAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	APlayerAIController();
+};
